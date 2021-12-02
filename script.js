e613033ab3aae515b31d901d6b5ba9f881af1a95
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-02 11:39:45 -0300

    add else if

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function carregar() {
-var msg = document.getElementById('msg');
-var img = document.getElementById('imagem');
+    var msg = document.getElementById('msg')
+    var img = document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+     if (hora >= 0 && hora < 12) {
+          //BOM DIA
+     }else if (hora >=12 && hora <18) {
+          //BOA TARDE
+     }else {
+          //BOA NOITE
+     }
 }
