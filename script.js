68ae569504e959a7369b2d3c1f13dad6b14f19a7
Marcos Antônio Campos Filho <marcoscampos96@hotmail.com>
2021-12-02 12:00:12 -0300

    add colors
    change background color base on hours

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ function carregar() {
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
-     if (hora >= 0 && hora < 12) {
-          //BOM DIA
-     }else if (hora >=12 && hora <18) {
-          //BOA TARDE
-     }else {
-          //BOA NOITE
-     }
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA
+        img.src = "media/manha-250.png"
+        document.body.style.background = '#A0A97A'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE
+        img.src = "media/tarde-250.png" 
+        document.body.style.background = '#AD6626' 
+    } else {
+        //BOA NOITE
+        img.src = "media/noite-250.png"
+        document.body.style.background = '#1E3455'
+    }
 }
